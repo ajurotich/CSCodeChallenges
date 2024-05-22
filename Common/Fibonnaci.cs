05fbefbccb1144211105a49eface691d3122a478
Asche Jurotich <asche.jurotich@gmail.com>
2024-05-21 19:55:35 -0500

    Renaming & CodeChallenges2 & Common
    Renamed first challenge
    
    created and completed codechallenges2
    
    created  and filled common

---
+++ Common/Fibonnaci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common;
+
+public class Fibonnaci {
+
+	public static int FibIndex(int n) =>
+		(n < 2) ? n : FibIndex(n-1) + FibIndex(n-2) ;
+
+	public static int FibLessThan(int n) {
+		if(n <=0 ) return 0;
+		int i = 1;
+		
+		while (FibIndex(i) < n) i++;
+
+		return i;
+	}
+
+}
---
+++ Common/General.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Common;
+
+public class General {
+
+	public static void Header(string title) {
+		if(title == "")
+			return;
+
+		Console.Title = title;
+		Console.WriteLine("\n" + title);
+		for(int i = 0;i<title.Length;i++)
+			Console.Write("-");
+		Console.WriteLine("\n");
+	}
+
+	public static void Header(string title, string description) {
+		if(title == "") return;
+
+		Console.Title = title;
+		Console.WriteLine("\n" + title.ToUpper() + '\n');
+
+		Console.WriteLine(description);
+		for(int i = 0; i<description.Length && i<Console.WindowWidth; i++) Console.Write("-");
+		Console.WriteLine("\n");
+	}
+
+	public static void Footer() =>
+		Console.WriteLine("\n\n\n");
+
+	public static string Stringify<T>(T input) =>
+		JsonSerializer.Serialize(input, new JsonSerializerOptions { WriteIndented = true });
+
+}
